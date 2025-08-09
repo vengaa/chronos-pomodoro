1f871cc2e70aa2f3f4752df4c53ac19886d7a3f9
Luis Felipe Pereira de Lima <luisfelipeadm.ps@gmail.com>
2025-08-09 12:51:06 -0300

    Criado componente CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -14,6 +15,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
