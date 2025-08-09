dd4011f620abe1ce9cd74099a927de8f35f0f221
Luis Felipe Pereira de Lima <luisfelipeadm.ps@gmail.com>
2025-08-09 11:53:26 -0300

    Criado o componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
 
 export function App() {
-  return (
-    <>
-      <Heading>Teste heading props</Heading>
-    </>
-  );
+  return <></>;
 }
